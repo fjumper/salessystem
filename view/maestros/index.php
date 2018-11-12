7ecccd4dd2509b239ccf5357d4902bce17cbc59d
ahuanay <76231561@continental.edu.pe>
2018-11-11 23:22:46 -0500

    ProyectoBase

---
+++ view/maestros/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	
+	<!-- Enlazamos Bootstrap 4 CSS -->
+	<!-- Documentación https://getbootstrap.com/docs/4.1/getting-started/introduction/ -->
+	<link rel="stylesheet" href="/css/bootstrap.min.css">
+
+	<!-- Enlazamos Iconos de Fontawesome CSS -->
+	<!-- Documentación https://fontawesome.com/icons?d=gallery -->
+	<link rel="stylesheet" href="/css/fontawesome.min.css">
+
+	<!-- Enlazamos Alertify CSS -->
+	<!-- Documentación https://alertifyjs.com/ -->
+	<link rel="stylesheet" href="/css/alertify.min.css">
+	<link rel="stylesheet" href="/css/themes/default.min.css">
+
+	<title>Document</title>
+</head>
+<body>
+
+	<div>
+
+	</div>
+
+	<!-- JQuery JS -->
+	<script src="/js/jquery-3.3.1.min.js"></script>
+
+	<!-- Enlazamos POPPER JS -->
+	<script src="/js/popper.min.js"></script>
+
+	<!-- Enlazamos Bootstrap 4 JS -->
+	<script src="/js/bootstrap.min.js"></script>
+
+	<!-- Enlazamos Alertify JS -->
+	<script src="/js/alertify.min.js"></script>
+</body>
+</html>
